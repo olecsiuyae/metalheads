5bfd0cbb183ff503be3ec6eff7e110d951114766
Andriy <olecsiuyae@gmail.com>
2015-02-14 17:10:21 +0200

    Add html files.Create static.Add utls for htmls and views

--- translate/urls.py
+++ translate/urls.py
@@ -2,4 +2,6 @@ __author__ = 'andriy'
 from django.conf.urls import patterns, url
 from translate import views
 urlpatterns = patterns('',
-        url(r'^$', views.main, name='main'))
+        url(r'^$', views.main, name='main'),
+        url(r'^contacts/$', views.contacts, name='contacts'),
+        url(r'^search/$', views.search, name='search'))
--- translate/views.py
+++ translate/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views her
 def main (request):
     return render(request, "metalheads/main.html")
+def categories (request):
+    return render(request, "metalheads/categories.html")
+def translates (request):
+    return  render(request, "metalheads/translates.html")
+def contacts (request):
+    return render(request, "metalheads/contacts.html")
+def search (request):
+    return render(request, "metalheads/search.html")
